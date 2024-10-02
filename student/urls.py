c69a0467aab9b423422a70d0c8a652964f7b8fd8
Sangam-oos <sangamshrestha52@gmail.com>
2024-10-02 13:17:29 +0545

    first commit

---
+++ student/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from student import views
+
+urlpatterns = [
+    path('student/dashboard/',views.StudentDashboard.as_view(),name='student_dashboard'),
+    path('student/dashboard/<str:error>',views.StudentDashboard.as_view(),name='student_dashboard_error'),
+    path('many/courses/',views.AllCourses.as_view(),name='many_course'),
+    path('course/<slug:slug>/',views.SingleCourse.as_view(),name='single_course'),
+    path('read/course/<slug:slug>/chapter/<int:pk>/<str:chapterName>/',views.ReadCourse.as_view(),name='read_course'),
+    path('give/course/<slug:slug>/test/<int:pk>/<str:title>/',views.GiveTest.as_view(),name='give_test'),
+    path('student/contact/',views.StudentContact.as_view(),name='student_contact'),
+    path('verify/payment/',views.VerifyPaymentView.as_view(),name='initiate_payment'),
+    path('cart/add/',views.AddToCartView.as_view(),name='add_to_cart'),
+    path('cart/',views.StudentCartView.as_view(),name='student_cart'),
+    path('cart/delete/<int:pk>',views.StudentCartDeleteVIew.as_view(),name='delete_from_cart')
+]
